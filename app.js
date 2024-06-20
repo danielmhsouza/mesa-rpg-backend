5124bb6de6d68f6ec922a815fe133b070df2e15f
Daniel Miguel <danielmiguel@Mac-mini-de-Daniel.local>
2024-06-19 23:56:32 -0300

    test route

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from express;
+const express = require('express');
 
 const PORT = 3333;
 
@@ -6,5 +6,8 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/teste", (req, res)=>{
+    return res.json({message: "it is running."});
+})
 
 app.listen(PORT, ()=>{console.log(`running: http://localhost:${PORT}`)});
